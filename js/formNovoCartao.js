7c482f8234052d7597e1b04165d362455915edfa
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-12-11 18:53:10 -0200

    Aula 1. Inicio formulário novo cartao

---
+++ js/formNovoCartao.js
@@ -0,0 +1,12 @@
+;(function(){
+    
+    const formulario = document.querySelector('.formNovoCartao');
+
+    formulario.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        console.log('clicou!');
+        
+    })
+    
+})()
---
+++ js/opcoesDoCartao/opcoes.js
@@ -0,0 +1,50 @@
+;(function(){
+
+    const cartoes = document.querySelectorAll('.cartao');
+
+    for(let cartao of cartoes) {
+        
+        cartao.addEventListener('focusin', function(){
+            this.classList.add('cartao--focado')
+        })
+
+        cartao.addEventListener('focusout', function(){
+            this.classList.remove('cartao--focado')
+        })
+
+        cartao.addEventListener('change', function(event){
+
+            const mudaCor = event.target.classList.contains('opcoesDoCartao-radioTipo')
+            
+            if(mudaCor){
+                this.style.backgroundColor = event.target.value;
+            }
+        })
+
+        cartao.addEventListener('keyup', function(event){
+
+            const mudaCor = event.target.classList.contains('opcoesDoCartao-tipo')
+            
+            if (mudaCor && (event.code == 'Enter' || event.code == 'Space' || event.code == 'NumpadEnter')){
+                
+                //this.style.backgroundColor = event.target.style.color;
+
+                event.target.click();
+
+            }
+            
+        })
+
+        cartao.addEventListener('click', function (event) {
+            if (event.target.classList.contains('opcoesDoCartao-remove')){
+                cartao.classList.add('cartao--some');
+
+                cartao.addEventListener('transitionend', function(){
+                    this.remove();
+                })
+            }
+        })
+        
+    }
+
+})()
